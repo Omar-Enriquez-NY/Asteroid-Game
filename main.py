cbf0c6c1cdacbadd46007a00fd2996b7f2113cb9
Omar-Enriquez-NY <omarxenriquez14@gmail.com>
2026-06-03 13:26:38 -0400

    3: Moves around

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ def main():
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_one = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    
     print("Starting Asteroids with pygame version: VERSION")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
 
 
     while(True):
+        dt = clock.tick(60) / 1000
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.update(dt)
         player_one.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
         print(dt)
 
 
