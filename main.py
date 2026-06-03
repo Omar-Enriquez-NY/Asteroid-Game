f381809189aa697205cdd1b963e1b9a6ba7f28d9
Omar-Enriquez-NY <omarxenriquez14@gmail.com>
2026-06-02 22:28:00 -0400

    2: Added the Circles and Drew the Player

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_one = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+
+
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.draw(screen)
         pygame.display.flip()
-
-
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
